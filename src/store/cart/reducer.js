a4f2891455ea4d564a2a67a340a42882961a6afc
Aditya Ramday <87653433+ramd24@users.noreply.github.com>
2022-04-24 23:08:36 +0700

    initial

---
+++ src/store/cart/reducer.js
@@ -0,0 +1,41 @@
+const reducer = (state = [], action) => {
+    switch (action.type){
+        case 'addCartItem':
+            {
+                const newState = [...state]
+                const itemIndex = newState.findIndex( (item) => item.id === action.payload.id )
+                if (itemIndex < 0){
+                    newState.push({
+                        id: action.payload.id,
+                        title: action.payload.title,
+                        price: action.payload.price,
+                        count: 1
+                    })
+                }else{
+                    newState[itemIndex].count += 1
+                }
+                return newState
+            }
+        case 'lessCartItem':
+            {
+                const newState = [...state]
+                const itemIndex = newState.findIndex( (item) => item.id === action.payload.id )
+                if ( newState[itemIndex].count <= 1 ){
+                    newState.splice(itemIndex, 1)
+                }else{
+                    newState[itemIndex].count -= 1
+                }
+                return newState
+            }
+        case 'removeCartItem':
+            {
+                const newState = [...state]
+                newState.splice(action.payload.index, 1)
+                return newState
+            }
+        default:
+            return state
+    }
+}
+
+export default reducer
